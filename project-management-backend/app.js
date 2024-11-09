b3e49adcd204c634370cba019978f55cdeca1b72
riyaraghuwanshi31 <127579773+riyaraghuwanshi31@users.noreply.github.com>
2024-11-09 15:18:40 +0530

    Update app.js

--- project-management-backend/app.js
+++ project-management-backend/app.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: ["https://real-trust-alpha.vercel.app/"],
+        origin: ["https://real-trust-alpha.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     }
